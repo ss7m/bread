d23aeea72f400005f4727c1a28b89c50da6ed46d
sam-barr <mail@samf.bar>
2021-01-14 15:08:22 -0500

    Parsing test

--- main.c
+++ main.c
@@ -2,53 +2,19 @@
 #include "ast.h"
 #include "vm.h"
 #include "token.h"
+#include "parse.h"
 
 int main(void)
 {
-        struct brd_node *un, *var, *num, *str, *b, *unit, *bin1, *bin2, *bin3, *ass;
-        struct brd_value_map map;
-        struct brd_value val;
-        struct brd_token_list list;
+        char *test_string = "set x = (2.8 + \n y * \n\n-4e9 = 200) \n or (set ab_r = not true and false)";
+        struct brd_token_list list, copy;
+        struct brd_node *node;
 
-        /* test ast node */
-        var = brd_node_var_new(strdup("x"));
-        num = brd_node_num_lit_new(123);
-        str = brd_node_string_lit_new(strdup("Hello, World!"));
-        b = brd_node_bool_lit_new(true);
-        unit = brd_node_unit_lit_new();
-
-        bin1 = brd_node_binop_new(BRD_PLUS, num, str);
-        un = brd_node_unary_new(BRD_NOT, bin1);
-        /* ass = brd_node_assign_new(un ,var) */ /* fails b/c un isn't an lvalue */
-        ass = brd_node_assign_new(var, un);
-        bin2 = brd_node_binop_new(BRD_PLUS, b, unit);
-        bin3 = brd_node_binop_new(BRD_PLUS, ass, bin2);
-
-        brd_node_destroy(bin3);
-
-        /* test hash map */
-        val.vtype = BRD_VAL_NUM;
-        val.as.num = 123;
-        brd_value_map_init(&map);
-        brd_value_map_set(&map, "abc", &val);
-        brd_value_map_set(&map, "hello", &val);
-        brd_value_map_set(&map, "world", &val);
-        brd_value_map_set(&map, "xyz", &val);
-        brd_value_map_set(&map, "test", &val);
-        brd_value_map_set(&map, "test", &val);
-        brd_value_map_set(&map, "here", &val);
-
-        printf("%Lf\n", brd_value_map_get(&map, "test")->as.num);
-        printf("%s\n", brd_value_map_get(&map, "here") ? "true" : "false");
-        printf("%s\n", brd_value_map_get(&map, "not here") ? "true" : "false");
-
-        brd_value_map_destroy(&map);
-
-        /* test token list */
         brd_token_list_init(&list);
-        brd_token_list_tokenize(&list, "123.5 + - <= > ( ) \n = < >=");
-        brd_token_list_tokenize(&list, "false true and set or sdf_e312_dD   ");
-        brd_token_list_tokenize(&list, "\"Hello, World!\\n\" 33422342.23  ");
-        /* brd_token_list_tokenize(&list, "\"Hello, World!\\n "); */ /* parsing error */
-        brd_token_list_destroy(&list);
+        brd_token_list_tokenize(&list, test_string);
+        copy = list;
+        node = brd_parse_expression(&list);
+        
+        brd_node_destroy(node);
+        brd_token_list_destroy(&copy);
 }
