7027063b5b2ebb5bb7d9cd24282e8d172ca8833e
sam-barr <mail@samf.bar>
2021-01-14 16:17:04 -0500

    minor change

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include "token.h"
 #include "parse.h"
 
-int main(void)
+int
+main(void)
 {
         char *test_string = "set x = (2.8 + \n y * \n\n-4e9 = \"Hello, World!\\n\") or (set ab_r = not true and false) \n\n set zd_d = 8\n 1 \n 1 \n 1 \n 1 \n 1 \n 1 \n 1 \n 1 \n 1\n1";
         struct brd_token_list list, copy;
