41cdba2c26b56f49d389340df676a2a5b5a5ebd1
sam-barr <mail@samf.bar>
2021-01-17 12:05:55 -0500

    accept file from command line

--- main.c
+++ main.c
@@ -5,16 +5,36 @@
 #include "parse.h"
 
 int
-main(void)
+main(int argc, char **argv)
 {
-        char *test_string = "@writeln(\"Hello\" \n, \", World!\", )";
+        FILE *file;
+        char *code;
+        size_t file_length;
+
         struct brd_token_list list, copy;
         struct brd_node_program *program;
         void *bytecode;
         struct brd_vm vm;
 
+
+        if (argc != 2) {
+                BARF("Enter exactly 1 argument");
+        }
+
+        file = fopen(argv[1], "r");
+        if (file == NULL) {
+                BARF("File %s doesn't exist", argv[1]);
+        }
+        fseek(file, 0, SEEK_END);
+        file_length = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        code = malloc(file_length + 1);
+        fgets(code, file_length, file);
+        fclose(file);
+
         brd_token_list_init(&list);
-        brd_token_list_tokenize(&list, test_string);
+        brd_token_list_tokenize(&list, code);
+        free(code);
         copy = list;
 
         program = brd_parse_program(&list);
